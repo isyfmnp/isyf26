7eaf57e322475adb789d676f95d0b6c9ffd166d1
lor <gnw832@gmail.com>
2025-11-25 13:17:37 +0800

    update keynote speaker and goh

--- pages/profiles/speakers.vue
+++ pages/profiles/speakers.vue
@@ -89,28 +89,23 @@ const profiles = [
     event: "Keynote Lecture",
     profiles: [
       {
+        name: "Prof Brian Schmidt",
+        status: "Keynote Speaker",
+        image: "brian-schmidt",
+        description:
+        "Brian Schmidt is Distinguished Professor of Astronomy at the Australian National University, of which he has been a faculty member since 1995. From 2016 to 2023, he was the university’s 12th Vice Chancellor and President. He is a Fellow of the Australian Academy of Science, the US National Academy of Sciences and the Royal Society (London), as well as a Foreign Member of the Spanish Royal Academy of Sciences and the Indian Academy of Sciences. He also serves on a number of boards and councils, including Australia’s National Science and Technology Council and Singapore’s Research Innovation and Enterprise Council.<br><br>Prof Schmidt has worked across many areas of astronomy, including supernovae, gamma-ray bursts, gravitational wave transients, exo-planets and metal-poor stars. In 2000, he received the Australian Government’s inaugural Malcolm McIntosh Award for achievement in the physical sciences. This was followed by the 2006 Shaw Prize for Astronomy and the 2007 Gruber Prize for Cosmology. For his work on the accelerating universe, he shared the 2011 Nobel Prize in Physics with Adam Riess and Saul Perlmutter. In 2013, he was made a Companion of the Order of Australia."
+      }
+    ],
+  },
+  {
+    event: "Guest of Honour",
+    profiles: [
+      {
         name: "Dr Ong Chen Hui",
         status: "Guest of Honour",
         image: "ong-chen-hui",
         description:
           "Dr Ong Chen Hui is the Assistant Chief Executive of the Business and Technology at the Infocomm Media Development Authority (IMDA), where she oversees the agency’s efforts in developing the industry and research ecosystem around emerging digital technologies, such as AI, AI safety, quantum communications and green computing. She developed the A.I. Verify toolkit and Project Moonshot for governance of AI, launched the National Multimodal LLM R&D programme that supports SEA-LION and MERaLiON LLMs, Future Communications R&D Programme to support 6G research, Digital Trust Centre to support research into trust technologies and National Quantum Safe Networks Plus Programme to support business access to quantum safe communications.<br><br>Chen Hui has spent more than 20 years in R&D and technology innovation. She serves on the technical boards of non-profits and is the Chair of AI Verify Foundation. Prior to IMDA, she was the APJ CTO in Trustwave – a Singtel company and Principal Member of Technical Staff in DSO National Laboratories.",
-      },
-      {
-        name: "Prof Brian Schmidt",
-        status: "Keynote Speaker",
-        image: "brian-schmidt",
-        description:
-        "(needs writing)"
-      }
-    ],
-  },
-  {
-    event: "Guest Lectures",
-    profiles: [
-      {
-        name: "Tentative",
-        image: "sow-chorng-haur",
-        description: "tentative"
       },
       ]
   },
